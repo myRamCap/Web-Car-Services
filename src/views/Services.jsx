aa00333f02cec0f3cd02170733f9993bb38f32ad
RandyBoy <sambile.randy@gmail.com>
2023-04-13 11:34:34 +0800

    update title

--- src/views/Services.jsx
+++ src/views/Services.jsx
@@ -44,7 +44,7 @@ export default function Workout() {
   return (
     <div id="services">
       <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-        <h1 className='pb-2'>Services</h1>
+        <h1 className='pb-2'>Services randy</h1>
       </div> 
     <ServicesDataTable />
     </div>
