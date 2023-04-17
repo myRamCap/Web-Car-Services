17717838ddf7cece9533904f562a1c1f1def4ab5
Master <jhonrayangcon1423@gmail.com>
2023-04-17 09:06:21 +0800

    Create ServiceLogo.jsx

---
+++ src/views/pages/ServiceLogo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ServiceLogoDataTable from '../../components/dataTables/ServiceLogoDataTable'
+
+export default function ServiceLogo() {
+  return (
+    <div id="serviceLogo">
+      <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+        <h1 className='pb-2'>Services Logo</h1>
+      </div> 
+        <ServiceLogoDataTable />
+    </div>
+  )
+}
