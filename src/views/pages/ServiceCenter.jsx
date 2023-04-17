16dc0826dfd105bacf82539449094c87996ce047
Master <jhonrayangcon1423@gmail.com>
2023-04-17 09:06:24 +0800

    Create ServiceCenter.jsx

---
+++ src/views/pages/ServiceCenter.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ServiceCenterDataTable from '../../components/dataTables/ServiceCenterDataTable'
+
+export default function ServiceCenter() {
+  return (
+    <div id="service_center">
+        <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+            <h1 className='pb-2'>Service Center</h1>
+        </div> 
+        <ServiceCenterDataTable />
+    </div>
+  )
+}
