31409c0125a3b06593593963cc86ccf284b1af79
RandyBoy <sambile.randy@gmail.com>
2023-04-19 08:22:14 +0800

    from Save to Edit

--- src/components/dataTables/ServicesDataTable.jsx
+++ src/components/dataTables/ServicesDataTable.jsx
@@ -32,7 +32,7 @@ export default function ServicesDataTable() {
     },
     {
       icon: () => <div className="btn btn-success btn-sm"><EditIcon  /></div> ,
-      tooltip: 'Save User',
+      tooltip: 'Edit User',
       onClick: (event) => setShowModal(true)
     }
   ]
