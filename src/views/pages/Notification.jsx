93719170e24e905758ac4574986f75eacfed7909
RandyBoy <sambile.randy@gmail.com>
2023-04-19 16:55:21 +0800

    Update Notification.jsx

--- src/views/pages/Notification.jsx
+++ src/views/pages/Notification.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import NotificationDataTable from '../../components/dataTables/NotificationDataTable'
 
 export default function Notification() {
   return (
-    <div><h1>Notifications</h1></div>
+    <div id="notification">
+      <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+        <h1 className='pb-2'>Notifications</h1>
+      </div> 
+        <NotificationDataTable />
+    </div>
   )
 }
